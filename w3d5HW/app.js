1a2556f769976827840a948f76f4ab35dde0ba81
Matt <msm3390@gmail.com>
2021-04-18 23:07:36 -0400

    first push

---
+++ w3d5HW/app.js
@@ -0,0 +1,44 @@
+//2. Create two variables that have numbers as their values
+var w = 7;
+var x = 15;
+//3. Create a function that returns the sum of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+function thisFunction(w, x){
+console.log(w + x);
+};
+//4. Create a function that returns the difference of two numbers that are arguments. Then console.log the function with the variables from step two as your two arguments.
+
+function thisFunction(x, w){
+    console.log(x - w);
+   };
+
+//5. Create a function that returns two arguments multiplied by each other. Then console.log the function with the variables from step two as your two arguments.
+
+function thisFunction(x, w){
+    console.log(x * w);
+   };
+ 
+
+//6. Create a function that returns divides the first argument by the second argument and returns the new value. Then console.log the function with the variables from step two as your two arguments.
+function thisFunction(x, w){
+    console.log(x / w);
+   };
+//7. Take the four functions that you created above and turn them into Arrow Functions, do not delete the original functions made, please have the original function and the arrow function on your completed homework. You should have a total of eight functions, four regular and four arrow functions.
+addEmUp = (x, w) => { return x + w};
+console.log(addEmUp(15, 7));
+
+subtractMe = (x, w) => { return x - w};
+console.log(subtractMe(15, 7));
+
+
+multiplyMe = (x, w) => { return x * w};
+console.log(multiplyMe(15, 7));
+
+divideMe = (x, w) => { return x / w};
+console.log(divideMe(15, 7));
+
+
+ 
+//8. Once you are done, deploy your project to GitHub and submit a link to your repository.
+  // ES5
+
+ 
